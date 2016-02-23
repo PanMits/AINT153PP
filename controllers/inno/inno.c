0ca2fb78655dd2212cbd996a2e499fefc6b95970
Sam Holmes <samholmes1337@gmail.com>
2016-02-23 16:38:30 +0000

    Create initial controller file

---
+++ controllers/inno/inno.c
@@ -0,0 +1,15 @@
+/*
+ * File:           inno.c
+ * Date:           23/02/2016
+ * Description:    Robot controller for navigating competition map
+ * Author:         Sam Holmes, Panos Mitseas, Paul Holland
+ */
+
+// Dependencies
+#include <webots/robot.h>
+#include <webots/motor.h>
+#include <webots/compass.h>
+#include <webots/gps.h>
+#include <webots/distance_sensor.h>
+#include <stdio.h>
+#include <math.h>
