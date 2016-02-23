e4ff470bfe035137d3419ae9858b29ce422dd0f8
Sam Holmes <samholmes1337@gmail.com>
2016-02-23 17:16:48 +0000

    Remove unwanted files and create .gitignore for builds

--- controllers/inno/build/release/inno.d
+++
@@ -1,8 +0,0 @@
-build/release/inno.o: inno.c \
-  /Applications/Webots/include/controller/c/webots/robot.h \
-  /Applications/Webots/include/controller/c/webots/types.h \
-  /Applications/Webots/include/controller/c/webots/nodes.h \
-  /Applications/Webots/include/controller/c/webots/motor.h \
-  /Applications/Webots/include/controller/c/webots/compass.h \
-  /Applications/Webots/include/controller/c/webots/gps.h \
-  /Applications/Webots/include/controller/c/webots/distance_sensor.h
