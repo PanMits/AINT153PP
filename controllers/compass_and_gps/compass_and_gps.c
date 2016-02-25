4327a4170857588a5786853c4f2593acf60f5781
Sam Holmes <samholmes1337@gmail.com>
2016-02-25 12:20:37 +0000

    Begin to implement mechanics for compass navigation

--- controllers/compass_and_gps/compass_and_gps.c
+++ controllers/compass_and_gps/compass_and_gps.c
@@ -4,15 +4,15 @@
  * Description:   A simple controller demonstrating the compass and the gps sensor. Drives along desired angle indicated.
  * Author:        Martin F. Stoelen
  */
- 
+
 
 /*
  * You may need to add include files like <webots/distance_sensor.h> etc.
  */
 #include <webots/robot.h>
-#include <webots/motor.h> 
-#include <webots/compass.h> 
-#include <webots/gps.h> 
+#include <webots/motor.h>
+#include <webots/compass.h>
+#include <webots/gps.h>
 #include <stdio.h>
 #include <math.h>
 
@@ -27,7 +27,7 @@
 
 
 /*
- * An example for converting bearing vector from compass to angle (in degrees). 
+ * An example for converting bearing vector from compass to angle (in degrees).
  */
 double convert_bearing_to_degrees(const double* in_vector) {
   double rad = atan2(in_vector[0], in_vector[2]);
@@ -41,8 +41,8 @@ int main(int argc, char **argv)
 
   /* necessary to initialize webots stuff */
   wb_robot_init();
-  
-  
+
+
   /*
   * You should declare here DeviceTag variables for storing
   * robot devices like this:
@@ -51,12 +51,12 @@ int main(int argc, char **argv)
   */
   WbDeviceTag left_wheel  = wb_robot_get_device("left_wheel");
   WbDeviceTag right_wheel = wb_robot_get_device("right_wheel");
-  
+
   WbDeviceTag compass = wb_robot_get_device("compass");
-  wb_compass_enable(compass, TIME_STEP); 
+  wb_compass_enable(compass, TIME_STEP);
   WbDeviceTag gps = wb_robot_get_device("gps");
-  wb_gps_enable(gps, TIME_STEP); 
-  
+  wb_gps_enable(gps, TIME_STEP);
+
   /*
   * Prepare robot for velocity commands
   */
@@ -64,16 +64,16 @@ int main(int argc, char **argv)
   wb_motor_set_position(right_wheel, INFINITY);
   wb_motor_set_velocity(left_wheel, 0.0);
   wb_motor_set_velocity(right_wheel,0.0);
-  
-  
-  /* 
+
+
+  /*
   * Main loop.
   * Perform a simulation step of 64 milliseconds
   * and leave the loop when the simulation is over
   */
   while (wb_robot_step(TIME_STEP) != -1) {
-  
-    /* 
+
+    /*
      * Read the sensors:
      * Enter here functions to read sensor data, like:
      *  double val = wb_distance_sensor_get_value(my_sensor);
@@ -81,14 +81,14 @@ int main(int argc, char **argv)
     const double *compass_val = wb_compass_get_values(compass);
     const double *gps_val = wb_gps_get_values(gps);
 
-    /* 
+    /*
      * Convert compass bearing vector to angle, in degrees.
      * Print GPS values.
     */
     double compass_angle = convert_bearing_to_degrees(compass_val);
     printf("Compass angle (degrees): %.2f.\n", compass_angle);
     printf("GPS values (x,z): %.2f, %.2f.\n", gps_val[0], gps_val[2]);
-    
+
     /*
     * Enter here functions to send actuator commands.
     */
@@ -111,15 +111,15 @@ int main(int argc, char **argv)
         right_speed = MAX_SPEED;
       }
     }
-    
+
     /*Set the motor speeds.*/
     wb_motor_set_velocity(left_wheel, left_speed);
     wb_motor_set_velocity(right_wheel, right_speed);
-    
+
   }
-  
+
   /* Necessary to cleanup webots stuff */
   wb_robot_cleanup();
-  
+
   return 0;
 }
--- controllers/inno/inno.c
+++ controllers/inno/inno.c
@@ -15,6 +15,15 @@
 // Macros
 #define TIME_STEP 64
 #define MAX_SPEED 3.0
+#define PI 3.14159265
+#define DESIRED_ANGLE -90.0
+
+// Convert bearing vector from compass to angle
+double convert_bearing_to_degrees(const double* in_vector) {
+  double rad = atan2(in_vector[0], in_vector[2]);
+  double deg = rad * (180.0 / PI);
+  return deg;
+}
 
 // Main function
 int main(int argc, char **argv)
@@ -35,6 +44,10 @@ int main(int argc, char **argv)
   WbDeviceTag right_sensor = wb_robot_get_device("so6");
   wb_distance_sensor_enable(right_sensor, TIME_STEP);
   
+  // Get the compass
+  WbDeviceTag compass = wb_robot_get_device("compass");
+  wb_compass_enable(compass, TIME_STEP);
+  
   // Prepare robot for velocity commands
   wb_motor_set_position(left_wheel, INFINITY);
   wb_motor_set_position(right_wheel, INFINITY);
@@ -52,32 +65,54 @@ int main(int argc, char **argv)
     double forward_left_value = wb_distance_sensor_get_value(forward_left_sensor);
     double forward_right_value = wb_distance_sensor_get_value(forward_right_sensor);
     double right_value = wb_distance_sensor_get_value(right_sensor);
+      
+    // Read compass and convert to angle
+    const double *compass_val = wb_compass_get_values(compass);
+    double compass_angle = convert_bearing_to_degrees(compass_val);
+    
+    // Debug
     printf("Sensor input values:\n");
     printf("- Forward left: %.2f.\n",forward_left_value);
     printf("- Forward right: %.2f.\n",forward_right_value);
     printf("- Right: %.2f.\n",right_value);
+    printf("- Compass angle (degrees): %.2f.\n", compass_angle);
     
     // Send acuator commands
     double left_speed, right_speed;
     left_speed = 0;
     right_speed = 0;
     
+    // List the current modes
     printf("Mode: %d.\n", mode);
     printf("Action: ");
-    if(mode == 0) { // Mode 0: Move forward
+    
+    /*
+     * There are four modes for this controller.
+     * They are listed as below:
+     * 0: Finding initial correct angle
+     * 1: Moving forward
+     * 2: Wall following
+     * 3: Finding green circle
+     */
+    
+    if (mode == 0) { // Mode 0: Find correct angle
+    
+      printf("Finding correct angle\n");
+    
+    } else if(mode == 1) { // Mode 1: Move forward
     
       printf("Moving forward.\n");
     
       left_speed = MAX_SPEED;
       right_speed = MAX_SPEED;
-      
+            
       // When sufficiently close to a wall in front of robot, switch mode to wall following
       if ((forward_right_value > 500) || (forward_left_value > 500)) {
-        mode = 1;
+        mode = 2;
       }
       
     } 
-    else { // Mode 1: Wall following
+    else if (mode == 2) { // Mode 2: Wall following
         
       if ((forward_right_value > 200) || (forward_left_value > 200)) {
         printf("Backing up and turning left.\n"); 
